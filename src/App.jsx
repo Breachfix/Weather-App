f5ffacedf5471e48b5fb009c094f74b871535bbe
Breachfix <Ntihanirahojacques@gmail.com>
2024-10-15 19:58:04 -0700

    Level 8: Add day/night mode toggle with persistent theme preference

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
 // src/App.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { WeatherProvider } from './context/WeatherContext';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import './assets/styles/App.css';
 
-const App = () => (
-  <WeatherProvider>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  </WeatherProvider>
-);
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => localStorage.getItem('theme') === 'dark');
+
+  useEffect(() => {
+    document.body.className = isDarkMode ? 'dark' : 'light';
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
+  const toggleTheme = () => setIsDarkMode((prevMode) => !prevMode);
+
+  return (
+    <WeatherProvider>
+      <Router>
+        <Navbar />
+        <div className="theme-toggle" onClick={toggleTheme}>
+          <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </WeatherProvider>
+  );
+};
 
 export default App;
